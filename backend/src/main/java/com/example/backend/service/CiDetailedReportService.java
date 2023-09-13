66e54974d2d900725596b2e64ea3e6e02d8f37e0
TRXCXS <2944859506@qq.com>
2023-09-13 23:40:03 +0800

    实现根据smId获得checkItem和对应的ciDetailedReportList

--- backend/src/main/java/com/example/backend/service/CiDetailedReportService.java
+++ backend/src/main/java/com/example/backend/service/CiDetailedReportService.java
@@ -4,6 +4,9 @@ import com.example.backend.entity.CiDetailedReport;
 import com.github.yulichang.base.MPJBaseService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface CiDetailedReportService extends MPJBaseService<CiDetailedReport> {
+    List<CiDetailedReport> getCheckItemDetailedReportByCheckItemId(Integer checkItemId);
 }
--- backend/src/main/java/com/example/backend/service/impl/CiDetailedReportServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/CiDetailedReportServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.backend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.backend.entity.CiDetailedReport;
 import com.example.backend.mapper.CiDetailedReportMapper;
 import com.example.backend.service.CiDetailedReportService;
@@ -7,8 +8,17 @@ import com.github.yulichang.base.MPJBaseServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CiDetailedReportServiceImpl extends MPJBaseServiceImpl<CiDetailedReportMapper, CiDetailedReport> implements CiDetailedReportService {
     private final CiDetailedReportMapper ciDetailedReportMapper;
+
+    @Override
+    public List<CiDetailedReport> getCheckItemDetailedReportByCheckItemId(Integer checkItemId) {
+        return ciDetailedReportMapper.selectList(
+                new QueryWrapper<CiDetailedReport>().eq("ciId", checkItemId)
+        );
+    }
 }
